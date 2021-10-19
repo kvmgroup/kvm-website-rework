27c4702f39057f2b36dd98d7a549a3eaa5a271a6
KIVOX <50070761+kivox@users.noreply.github.com>
2021-10-19 18:06:58 +0100

    Update our-team.js

--- pages/company/our-team.js
+++ pages/company/our-team.js
@@ -40,8 +40,10 @@ export default function Team() {
                     </Member>
 		    <Member name="Petar Markov" position="Chief Information Officer" image="petar.png">
                         <Email address="petar@kvm.group" />
+                        <Instagram handle="leifursteinng" />
                         <Twitter handle="kiv0x" />
                         <GitHub handle="kivox" />
+                        <LinkedIn handle="kjartanhr" />
                     </Member>
                     <Member name="Jack Cosens" position="Chief Financial Officer" image="jack.png">
                         <Email address="jack@kvm.group" />
